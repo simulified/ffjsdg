dd0d03845d64bb9fea4dff9310336c0a25dd8407
stan <51496538+stan2474@users.noreply.github.com>
2023-08-27 02:11:34 -0400

    update credits page

--- resources/views/documents/credits.blade.php
+++ resources/views/documents/credits.blade.php
@@ -7,7 +7,7 @@
     <div class="card shadow-sm">
         <div class="card-header">{{ config('app.name') }} Credits</div>
         <div class="card-body">
-            <h1>{{ config('app.name') }} Developers</h1>
+            <h1>Tadah Developers</h1>
             <ul>
                 <li><b>kinery</b> - Project Lead</li>
                 <li><b>spike</b> - Tadah lead artist, designed the logo and Token icon</li>
@@ -21,14 +21,19 @@
             <h4>Special thanks</h4>
             <ul>
                 <li><b>Anonymous</b> - Helped clean up code, client help</li>
-                <li><b>splat</b></a> - Ideas guy and helped found Tadah</li>
-                <li><b>past</b></a> - Catalog upload, event staff</li>
-                <li><b>warden</b></a> - Catalog uploader, event host</li>
-                <li><b>cole</b></a> - Main catalog manager</li>
+                <li><b>splat</b> - Ideas guy and helped found Tadah</li>
+                <li><b>past</b> - Catalog upload, event staff</li>
+                <li><b>warden</b> - Catalog uploader, event host</li>
+                <li><b>cole</b> - Main catalog manager</li>
                 <li><b>You</b> - for using Tadah!</li>
             </ul>
+            <h1>Kapish Developers</h1>
+            <ul>
+                <li><b>Stan</b> - Project Lead</li>
+                <li><b>Calones</b> - Server manager and person who made client arbiter</li>
+            </ul>
 
-            <p>Without these people lending their help, {{ config('app.name') }} would not be as good as it is today. Thanks, everyone.</p>
+            <p>Without these people lending their help, {{ config('app.name') }} would not be as good as it is today. Thanks everyone.</p>
         </div>
     </div>
 </div>
